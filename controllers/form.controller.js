cad32a405fae24c6da5290c79e4306f85ce0a0fc
abraham-r-bytebandits <abraham.r@bytebandits.in>
2026-02-20 12:30:32 +0530

    correct the form controller

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -6,17 +6,6 @@ export const submitForm = async (req, res) => {
     if (!req.file) {
       return res.status(400).json({ error: "File is required" });
     }
-
-    return res.json({
-      message: "Form submitted successfully!",
-      body: req.body,
-      file: {
-        originalname: req.file.originalname,
-        filename: req.file.filename,
-        path: req.file.path,
-        size: req.file.size,
-      },
-    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Server error" });
